1f13fa381f4a62c7a46b41492c98f92b49ec5a1d
Your Name <you@example.com>
2025-04-23 01:46:18 +0500

    final resolve

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/my-portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
